cecb99b8f44559c4f022673f25d8223c74dfc8b5
Joel Filho <joelfilho02812@gmail.com>
2022-08-15 16:30:43 -0300

    add tela de formulário pra preenchimento de departamento

--- src/main/java/com/project_classes/gui/ListaDepertamentosController.java
+++ src/main/java/com/project_classes/gui/ListaDepertamentosController.java
@@ -1,18 +1,27 @@
 package com.project_classes.gui;
 
 import com.project_classes.application.Main;
+import com.project_classes.gui.util.Alerts;
+import com.project_classes.gui.util.Utils;
 import com.project_classes.model.entities.Department;
 import com.project_classes.model.services.ServicesDepartamento;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -36,8 +45,11 @@ public class ListaDepertamentosController implements Initializable {
 
 
     @FXML
-    public void onBtNewAction() {
-        System.out.println("onBtNewAction");
+    public void onBtNewAction(ActionEvent event) {
+
+        Stage parentStage = Utils.currentStage(event);
+
+        createDialogForm("/com/project_classes/FormDepartamentos.fxml", parentStage);
     }
 
     @Override
@@ -65,5 +77,24 @@ public class ListaDepertamentosController implements Initializable {
         obsList = FXCollections.observableArrayList(list);
 
         tableViewDepartment.setItems(obsList);
+    }
+
+    private void createDialogForm(String absoluteName, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+
+            dialogStage.setTitle("Coloque os dados do Departamento");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+
+        } catch (IOException e) {
+            Alerts.showAlert("IOException", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
---
+++ src/main/java/com/project_classes/gui/util/Utils.java
@@ -0,0 +1,13 @@
+package com.project_classes.gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+    public static Stage currentStage(ActionEvent event) {
+        return (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
+
+}
